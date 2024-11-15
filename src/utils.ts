fbef404e0cef27b8a77f219e10e6c91c4dbae86a
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-11-15 05:41:15 +0000

    Refactor type annotations in utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -4,10 +4,12 @@
  * @example
  * countOccurrences([1, 2], [2, 3]); // { 1: 1, 2: 2, 3: 1 }
  *
- * @param {...any[]} arrays - Arrays to be compared
+ * @param {...unknown[]} arrays - Arrays to be compared
  * @return {Record<string, number>} Array values and their occurrence count
  */
-export function countOccurrences(...arrays: any[][]): Record<string, number> {
+export function countOccurrences(
+  ...arrays: unknown[][]
+): Record<string, number> {
   const countObj: Record<string, number> = {};
 
   arrays.forEach(array => {
@@ -28,10 +30,10 @@ export function countOccurrences(...arrays: any[][]): Record<string, number> {
  * getInAll([1, 2, 3], [2, 3, 4], [3, 4, 5]); // [3]
  * getInAll([1, 2, 3, 'x'], [2, 3, 4, 'x'], [3, 4, 5]); // [3]
  *
- * @param {...any[]} arrays - Arrays to be compared
- * @return {any[]} List of values
+ * @param {...unknown[]} arrays - Arrays to be compared
+ * @return {unknown[]} List of values
  */
-export function getInAll(...arrays: any[][]): any[] {
+export function getInAll(...arrays: unknown[][]): unknown[] {
   return arrays.reduce((acc, curr) =>
     acc.filter(value => new Set(curr).has(value))
   );
@@ -45,10 +47,10 @@ export function getInAll(...arrays: any[][]): any[] {
  * getInMultiple([1, 2, 3], [2, 3, 4], [3, 4, 5]); // [2, 3, 4]
  * getInMultiple([1, 2, 3, 'x'], [2, 3, 4, 'x'], [3, 4, 5]); // [2, 3, 4, 'x']
  *
- * @param {...any[]} arrays - Arrays to be compared
- * @return {any[]} List of values
+ * @param {...unknown[]} arrays - Arrays to be compared
+ * @return {unknown[]} List of values
  */
-export function getInMultiple(...arrays: any[][]): any[] {
+export function getInMultiple(...arrays: unknown[][]): unknown[] {
   const countObj = countOccurrences(...arrays);
 
   return Object.keys(countObj)
@@ -64,10 +66,10 @@ export function getInMultiple(...arrays: any[][]): any[] {
  * getNotInAll([1, 2, 3], [2, 3, 4], [3, 4, 5]); // [1, 2, 4, 5]
  * getNotInAll([1, 2, 3, 'x'], [2, 3, 4, 'x'], [3, 4, 5]); // [1, 2, 4, 5, 'x']
  *
- * @param {...any[]} arrays - Arrays to be compared
- * @return {any[]} List of values
+ * @param {...unknown[]} arrays - Arrays to be compared
+ * @return {unknown[]} List of values
  */
-export function getNotInAll(...arrays: any[][]): any[] {
+export function getNotInAll(...arrays: unknown[][]): unknown[] {
   const countObj = countOccurrences(...arrays);
 
   return Object.keys(countObj)
@@ -83,10 +85,10 @@ export function getNotInAll(...arrays: any[][]): any[] {
  * getNotInMultiple([1, 2, 3], [2, 3, 4], [3, 4, 5]); // [1, 5]
  * getNotInMultiple([1, 2, 3, 'x'], [2, 3, 4, 'x'], [3, 4, 5]); // [1, 5]
  *
- * @param {...any[]} arrays - Arrays to be compared
- * @return {any[]} List of values
+ * @param {...unknown[]} arrays - Arrays to be compared
+ * @return {unknown[]} List of values
  */
-export function getNotInMultiple(...arrays: any[][]): any[] {
+export function getNotInMultiple(...arrays: unknown[][]): unknown[] {
   const countObj = countOccurrences(...arrays);
 
   return Object.keys(countObj)
@@ -129,7 +131,7 @@ export function getObjectKeys(
  * @param {*} value - Value to test
  * @return {boolean}
  */
-export function isObject(value: any): value is Record<string, any> {
+export function isObject(value: unknown): value is Record<string, unknown> {
   return typeof value === 'object' && value !== null && !Array.isArray(value);
 }
 
@@ -139,7 +141,7 @@ export function isObject(value: any): value is Record<string, any> {
  * @param {*} obj - Value to test
  * @return {boolean}
  */
-export function isPropDescriptor(obj: any): obj is PropertyDescriptor {
+export function isPropDescriptor(obj: unknown): obj is PropertyDescriptor {
   if (!isObject(obj)) {
     return false;
   }
@@ -178,7 +180,7 @@ export function isPropDescriptor(obj: any): obj is PropertyDescriptor {
  * @param key - The key to parse.
  * @returns The parsed value.
  */
-function parseValue(key: string): any {
+function parseValue(key: string): unknown {
   try {
     return JSON.parse(key);
   } catch {
